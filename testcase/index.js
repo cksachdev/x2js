d8f00efe7ba07e824a3329aef164e5862a105b16
Chetan Sachdev <cksachdev@gmail.com>
2019-01-23 03:09:33 +0530

    Getting x2js to work in nodejs using xmldom parser

--- testcase/index.js
+++ testcase/index.js
@@ -1,9 +1,9 @@
-const X2JS=require("./xml2json");
+const X2JS=require("../xml2json");
 const fs = require('fs-extra');
-var DomParser = require('dom-parser');
-global.parser = new DomParser();
-var text=fs.readFileSync('F:/code/diksha-implementation/x2js/index.ecml','utf8');
-var x2js = new X2JS();
+var text=fs.readFileSync('index.ecml','utf8');
+console.log(text);
+var x2js = new X2JS({ attributePrefix: 'none', enableToStringFunc: false });
 //var xmlText = "<MyRoot><test>Success</test><test2><item>val1</item><item>val2</item></test2></MyRoot>";
 var jsonObj = x2js.xml_str2json( text );
 console.log(jsonObj);
+fs.writeFileSync("output.json", JSON.stringify(jsonObj), 'utf8'); 
